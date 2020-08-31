eb4400602d0f53ba3e4cfb8ec60d65af4757f5d2
Michael Klepac <mklepac90@gmail.com>
2020-08-31 20:19:11 +0000

    added random string generator

--- express_server.js
+++ express_server.js
@@ -6,6 +6,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
+function generateRandomString(length) {
+  return Math.random().toString(36).substring(2, (length + 2));
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -35,6 +39,11 @@ app.get("/hello", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
